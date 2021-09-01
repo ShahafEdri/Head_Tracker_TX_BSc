3977ee7d8ba797d7565ff62858121aecb2cc43f4
Shahaf Edri <edri.sue@gmail.com>
2021-09-01 14:14:10 +0300

    minor changes

--- Examples and tests/sanity_test_nrf24.cpp
+++ Examples and tests/sanity_test_nrf24.cpp
@@ -6,27 +6,30 @@
 #include <printf.h>
 
 #define STM32F1
+
 /*-----( Declare Constants and Pin Numbers )-----*/
 #ifdef STM32F1
-#define  CE_PIN  PB1   // The pins to be used for CE and SN
-#define  CSN_PIN PB0
-#endif
-
-#ifdef ARDUINO_NANO // arduino pins
-#define  CE_PIN  7   // The pins to be used for CE and SN
-#define  CSN_PIN 8
+    #define  CE_PIN  PB1   // The pins to be used for CE and SN
+    #define  CSN_PIN PB0
+#elif #defined ARDUINO_NANO // arduino pins
+    #define  CE_PIN  7   // The pins to be used for CE and SN
+    #define  CSN_PIN 8
 #endif
 
 RF24 radio(CE_PIN, CSN_PIN);
 
 void setup() 
 {
- while (!Serial);
- Serial.begin(9600);
- printf_begin();
- radio.begin();
- radio.setDataRate(RF24_2MBPS);
- radio.setPALevel(RF24_PA_MIN);
+    while(radio.isChipConnected() == false)
+        Serial.println("chip is NOT connected");
+
+    Serial.println("chip not connected");
+    while (!Serial);
+    Serial.begin(9600);
+    printf_begin();
+    radio.begin();
+    radio.setDataRate(RF24_2MBPS);
+    radio.setPALevel(RF24_PA_MIN);
 }
 
 void loop()
